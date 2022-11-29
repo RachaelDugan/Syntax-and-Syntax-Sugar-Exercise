dd06b6bedd031ed04b859ada75736bf96b685b6e
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-11-29 13:47:30 -0700

    initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 10;
+            string response;
+
+            if (answer < 9)
+            {
+                response = (answer + " is less than nine.");
+            }
+            else 
+            {
+                response = (answer + " is greater than nine.");
+            }
+            Console.WriteLine(response);
+
+            var input = 7;
+            var output = (input < 9) ? $"{input} is less than nine" : $"{input} is greater than nine";
+            Console.WriteLine(output);
         }
     }
 }
